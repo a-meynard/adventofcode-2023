6c95b202fb969704b0e4802cd939a5225fe84d83
Antoine Meynard <work.ameynard@gmail.com>
2023-12-04 00:35:11 +0100

    feat(day3): add day3 and its acceptance test

---
+++ day3/tests/acceptance/test_main.py
@@ -0,0 +1,17 @@
+from main import Usecase
+
+
+def test_usecase_with_simple_input():
+    input = [
+        "467..114..",
+        "...*......",
+        "..35..633.",
+        "......#...",
+        "617*......",
+        ".....+.58.",
+        "..592.....",
+        "......755.",
+        "...$.*....",
+        ".664.598..",
+    ]
+    assert Usecase().solve_first_problem(puzzle_input=input) == 4361
