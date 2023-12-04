7144829a2c86972bd1649ce42b748c643d043d83
Antoine Meynard <work.ameynard@gmail.com>
2023-12-04 16:01:09 +0100

    feat(day3): add acceptance test for second problem of day3

--- day3/tests/acceptance/test_main.py
+++ day3/tests/acceptance/test_main.py
@@ -15,3 +15,19 @@ def test_usecase_with_simple_input():
         ".664.598..",
     ]
     assert Usecase().solve_first_problem(puzzle_input=input) == 4361
+
+
+def test_usecase_solve_second_problem():
+    input = [
+        "467..114..",
+        "...*......",
+        "..35..633.",
+        "......#...",
+        "617*......",
+        ".....+.58.",
+        "..592.....",
+        "......755.",
+        "...$.*....",
+        ".664.598..",
+    ]
+    assert Usecase().solve_second_problem(puzzle_input=input) == 467835
