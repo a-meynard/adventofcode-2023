3033f3e93d504fa4b1e2af275819d11e007f66d8
Antoine Meynard <work.ameynard@gmail.com>
2023-12-04 12:44:19 +0100

    feat(day3): add usecase to convert game puzzl into object understandble to FilterInvalidPartNumber

---
+++ day3/ingest_engine_schema.py
@@ -0,0 +1,65 @@
+from typing import Optional
+
+from engine_part import EnginePart
+from schema import Schema
+from part_number import PartNumber
+from position import Position
+
+
+def is_a_number(char: str) -> bool:
+    try:
+        int(char)
+        return True
+    except ValueError:
+        pass
+    return False
+
+
+class IngestEngineSchema:
+    VALID_SYMBOLS = [i for i in r'\/:*?"<>|#+$&@%=-']
+
+    def __init__(self) -> None:
+        pass
+
+    def handle(self, raw_schema: list[str]) -> Schema:
+        engine_parts: set[EnginePart] = set()
+        part_numbers: set[PartNumber] = set()
+        current_potential_part_number: Optional[PartNumber] = None
+
+        for y, line in enumerate(raw_schema):
+            for x, char in enumerate(line):
+                if char == ".":
+                    if current_potential_part_number is not None:
+                        part_numbers.add(current_potential_part_number)
+                        current_potential_part_number = None
+                elif is_a_number(char=char):
+                    if current_potential_part_number is not None:
+                        current_potential_part_number = PartNumber(
+                            id=int(
+                                line[
+                                    current_potential_part_number.position.start : x + 1
+                                ]
+                            ),
+                            position=Position(
+                                start=current_potential_part_number.position.start,
+                                end=x,
+                                line=y,
+                            ),
+                        )
+                    else:
+                        current_potential_part_number = PartNumber(
+                            id=int(char), position=Position(start=x, end=x, line=y)
+                        )
+                elif char in IngestEngineSchema.VALID_SYMBOLS:
+                    if current_potential_part_number is not None:
+                        part_numbers.add(current_potential_part_number)
+                        current_potential_part_number = None
+                    engine_parts.add(
+                        EnginePart(position=Position(start=x, end=x, line=y))
+                    )
+                else:
+                    raise ValueError(f"Found an unknown character, got: '{char}'")
+            if current_potential_part_number is not None:
+                part_numbers.add(current_potential_part_number)
+                current_potential_part_number = None
+        return Schema(symbols=engine_parts, part_numbers=part_numbers)
---
+++ day3/tests/unit/test_ingest_engine_schema.py
@@ -0,0 +1,76 @@
+from schema import Schema
+from engine_part import EnginePart
+from position import Position
+from part_number import PartNumber
+
+from ingest_engine_schema import IngestEngineSchema
+
+
+def test_ingest_engine_schema_with_only_space():
+    raw_schema = ["."]
+    usecase = IngestEngineSchema()
+
+    assert usecase.handle(raw_schema=raw_schema) == Schema(
+        symbols=set(),
+        part_numbers=set(),
+    )
+
+
+def test_ingest_engine_schema_with_only_single_digit_part_numbers():
+    raw_schema = ["1"]
+    usecase = IngestEngineSchema()
+
+    assert usecase.handle(raw_schema=raw_schema) == Schema(
+        symbols=set(),
+        part_numbers={
+            PartNumber(id=1, position=Position(start=0, end=0, line=0)),
+        },
+    )
+
+
+def test_ingest_engine_schema_with_only_engine_part():
+    raw_schema = ["*"]
+    usecase = IngestEngineSchema()
+
+    assert usecase.handle(raw_schema=raw_schema) == Schema(
+        symbols={EnginePart(position=Position(start=0, end=0, line=0))},
+        part_numbers=set(),
+    )
+
+
+def test_ingest_engine_schema_with_only_multi_digit_part_numbers():
+    raw_schema = ["123."]
+    usecase = IngestEngineSchema()
+
+    assert usecase.handle(raw_schema=raw_schema) == Schema(
+        symbols=set(),
+        part_numbers={
+            PartNumber(id=123, position=Position(start=0, end=2, line=0)),
+        },
+    )
+
+
+def test_ingest_engine_schema_with_multiple_lines():
+    raw_schema = ["123.", "*..0"]
+    usecase = IngestEngineSchema()
+
+    assert usecase.handle(raw_schema=raw_schema) == Schema(
+        symbols={EnginePart(position=Position(start=0, end=0, line=1))},
+        part_numbers={
+            PartNumber(id=123, position=Position(start=0, end=2, line=0)),
+            PartNumber(id=0, position=Position(start=3, end=3, line=1)),
+        },
+    )
+
+
+def test_ingest_engine_schema_with_symbol_between_part_numbers():
+    raw_schema = ["123*12"]
+    usecase = IngestEngineSchema()
+
+    assert usecase.handle(raw_schema=raw_schema) == Schema(
+        symbols={EnginePart(position=Position(start=3, end=3, line=0))},
+        part_numbers={
+            PartNumber(id=123, position=Position(start=0, end=2, line=0)),
+            PartNumber(id=12, position=Position(start=4, end=5, line=0)),
+        },
+    )
