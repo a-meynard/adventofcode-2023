eea37cf8d99e16251ee589c498c3261b94f61c2c
Antoine Meynard <work.ameynard@gmail.com>
2023-12-04 12:47:51 +0100

    feat(day3): solve first problem of day3

--- day3/main.py
+++ day3/main.py
@@ -1,14 +1,17 @@
-# note: object to consider
-# PotentialPartNumber
-# Part = symbol
+from ingest_engine_schema import IngestEngineSchema
+from filter_invalid_part_number import FilterInvalidPartNumber
 
 
 class Usecase:
     def __init__(self) -> None:
         pass
 
-    def solve_first_problem(self, puzzle_input: str):
-        pass
+    def solve_first_problem(self, puzzle_input: list[str]) -> int:
+        sanitized_puzzle_input = [line.replace("\n", "") for line in puzzle_input]
+        part_numbers = FilterInvalidPartNumber().handle(
+            schema=IngestEngineSchema().handle(raw_schema=sanitized_puzzle_input)
+        )
+        return sum([p.id for p in part_numbers])
 
     def solve_second_problem(self, puzzle_input: str):
         pass
