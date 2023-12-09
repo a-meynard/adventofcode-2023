083502e3c346faf820a1d866da716484be216d78
Antoine Meynard <work.ameynard@gmail.com>
2023-12-10 00:33:39 +0100

    feat(day5): add usecase to search for the lowest location in an almanac

---
+++ day5/tests/unit/test_search_lowest_location.py
@@ -0,0 +1,104 @@
+from almanac import Almanac
+from category import Category
+
+from typing import Optional
+
+
+class SearchLowestLocation:
+    def __init__(self) -> None:
+        self.category_types = []
+
+    def find_location(self, almanac: Almanac, seed: int) -> int:
+        next_stop = seed
+        for category_type in self.category_types:
+            for category in [c for c in almanac.categories if c.name == category_type]:
+                if category.number == next_stop:
+                    next_stop = category.destination_numer
+                    break
+        return next_stop
+
+    def handle(self, almanac: Almanac) -> int:
+        lowest_location: Optional[int] = None
+
+        # HACK: Here we use the fact that the Almanac probably has its category
+        # ordered in the correct way.
+        self.category_types = []
+        for category in almanac.categories:
+            # NOTE: Could be optimized with an OrderedSet data type
+            if category.name not in self.category_types:
+                self.category_types.append(category.name)
+
+        return min(
+            [
+                self.find_location(almanac=almanac, seed=seed)
+                for seed in almanac.initial_seeds
+            ]
+        )
+
+
+def test_search_lowest_location_with_one_seed_no_category():
+    almanac = Almanac(
+        initial_seeds=[79],
+        categories=[],
+    )
+    assert SearchLowestLocation().handle(almanac=almanac) == 79
+
+
+def test_search_lowest_location_with_one_seed_and_one_category_type():
+    almanac = Almanac(
+        initial_seeds=[79],
+        categories=[
+            Category(name="seed", number=79, destination_numer=50),
+        ],
+    )
+    assert SearchLowestLocation().handle(almanac=almanac) == 50
+
+
+def test_search_lowest_location_with_multiple_seed_and_no_category_type():
+    almanac = Almanac(
+        initial_seeds=[79, 14],
+        categories=[],
+    )
+    assert SearchLowestLocation().handle(almanac=almanac) == 14
+
+
+def test_search_lowest_location_with_multiple_seed_and_one_category_type():
+    almanac = Almanac(
+        initial_seeds=[79, 14],
+        categories=[
+            Category(name="seed", number=79, destination_numer=50),
+            Category(name="seed", number=14, destination_numer=44),
+        ],
+    )
+    assert SearchLowestLocation().handle(almanac=almanac) == 44
+
+
+def test_search_lowest_location_with_multiple_seed_and_one_category_type_and_useless_categories():
+    almanac = Almanac(
+        initial_seeds=[79, 16],
+        categories=[
+            Category(name="seed", number=79, destination_numer=50),
+            Category(name="seed", number=14, destination_numer=44),
+            Category(name="seed", number=15, destination_numer=45),
+            Category(name="seed", number=16, destination_numer=46),
+            Category(name="seed", number=17, destination_numer=47),
+        ],
+    )
+    assert SearchLowestLocation().handle(almanac=almanac) == 46
+
+
+def test_search_lowest_location_with_multiple_seed_and_multiple_category_types():
+    almanac = Almanac(
+        initial_seeds=[79, 16],
+        categories=[
+            Category(name="seed", number=79, destination_numer=50),
+            Category(name="seed", number=14, destination_numer=44),
+            Category(name="seed", number=15, destination_numer=45),
+            Category(name="seed", number=16, destination_numer=46),
+            Category(name="seed", number=17, destination_numer=47),
+            Category(name="soil", number=44, destination_numer=12),
+            Category(name="soil", number=45, destination_numer=13),
+            Category(name="soil", number=46, destination_numer=14),
+        ],
+    )
+    assert SearchLowestLocation().handle(almanac=almanac) == 14
